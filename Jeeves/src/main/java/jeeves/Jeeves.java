2c85943d0a7447f9666db7cfc9b5200994cf1648
Dimitris Sarigiannis <saridimi@gmail.com>
2014-08-20 09:11:47 -0700

    scp + http post have been added

--- Jeeves/src/main/java/jeeves/Jeeves.java
+++ Jeeves/src/main/java/jeeves/Jeeves.java
@@ -1,6 +1,5 @@
 package jeeves;
 
-import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.sql.Time;
@@ -8,6 +7,32 @@ import java.sql.Time;
 import org.json.JSONException;
 
 import jobdescriptor.JobDescriptor;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import java.util.Properties;
+
+import net.schmizz.sshj.SSHClient;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import com.jcraft.jsch.Channel;
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 
 /**
  * 
@@ -132,6 +157,188 @@ public class Jeeves {
 			jeeves.writeFairShipResultsToHDFS();
 		}
 
+	}
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		//unknown url at the moment
+		String url = "http://...";
+
+		HttpClient httpclient = HttpClientBuilder.create().build();
+		HttpPost post = new HttpPost(url);
+
+		// add header
+		post.setHeader("Content-Type", "application/root");
+		try {
+			post.setEntity(new StringEntity(
+					getContentOfFile(FairShipLocation + "/FairShip/" + "build/" + resultsFilename)));
+
+			HttpResponse response = httpclient.execute(post);
+			System.out.println("\nSending 'POST' request to URL : " + url);
+			System.out.println("Post parameters : " + post.getEntity());
+			System.out.println("Response Code : "
+					+ response.getStatusLine().getStatusCode());
+			
+			/* //print
+			BufferedReader rd = new BufferedReader(new InputStreamReader(
+					response.getEntity().getContent()));
+
+			StringBuffer result = new StringBuffer();
+			String line = "";
+			while ((line = rd.readLine()) != null) {
+				result.append(line);
+			}*/
+			
+			
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private String getContentOfFile(String filename) throws IOException {
+		FileInputStream inputStream = new FileInputStream(filename);
+		try {
+			String str = IOUtils.toString(inputStream);
+			return str;
+		} finally {
+			inputStream.close();
+		}
+	}
+
+	private void sendWithSCP() {
+		System.out.println("SCP START");
+
+		FileInputStream fis = null;
+		try {
+
+			String lfile = "FairRunInfo_ship.Pythia8-TGeant4.root";
+			String user = "username";
+
+			String host = "127.1.1.0";
+			String rfile = "TheRemoteTwo.txt";
+
+			JSch jsch = new JSch();
+			Session session = jsch.getSession(user, host, 22);
+			session.setPassword("********");
+
+			Properties config = new Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+
+			boolean ptimestamp = true;
+
+			// exec 'scp -t rfile' remotely
+			String command = "scp " + (ptimestamp ? "-p" : "") + " -t " + rfile;
+			Channel channel = session.openChannel("exec");
+			((ChannelExec) channel).setCommand(command);
+
+			// get I/O streams for remote scp
+			OutputStream out = channel.getOutputStream();
+			InputStream in = channel.getInputStream();
+
+			channel.connect();
+
+			if (checkAck(in) != 0) {
+				System.exit(0);
+			}
+
+			File _lfile = new File(lfile);
+
+			if (ptimestamp) {
+				command = "T " + (_lfile.lastModified() / 1000) + " 0";
+				// The access time should be sent here,
+				// but it is not accessible with JavaAPI ;-<
+				command += (" " + (_lfile.lastModified() / 1000) + " 0\n");
+				out.write(command.getBytes());
+				out.flush();
+				if (checkAck(in) != 0) {
+					System.exit(0);
+				}
+			}
+
+			// send "C0644 filesize filename", where filename should not include
+			// '/'
+			long filesize = _lfile.length();
+			command = "C0644 " + filesize + " ";
+			if (lfile.lastIndexOf('/') > 0) {
+				command += lfile.substring(lfile.lastIndexOf('/') + 1);
+			} else {
+				command += lfile;
+			}
+			command += "\n";
+			out.write(command.getBytes());
+			out.flush();
+			if (checkAck(in) != 0) {
+				System.exit(0);
+			}
+
+			// send a content of lfile
+			fis = new FileInputStream(lfile);
+			byte[] buf = new byte[1024];
+			while (true) {
+				int len = fis.read(buf, 0, buf.length);
+				if (len <= 0)
+					break;
+				out.write(buf, 0, len); // out.flush();
+			}
+			fis.close();
+			fis = null;
+			// send '\0'
+			buf[0] = 0;
+			out.write(buf, 0, 1);
+			out.flush();
+			if (checkAck(in) != 0) {
+				System.exit(0);
+			}
+			out.close();
+
+			channel.disconnect();
+			session.disconnect();
+			System.out.println("SCP END");
+			System.exit(0);
+		} catch (Exception e) {
+			System.out.println(e);
+			try {
+				if (fis != null)
+					fis.close();
+			} catch (Exception ee) {
+			}
+		}
+	}
+
+	private int checkAck(InputStream in) throws IOException {
+		int b = in.read();
+		// b may be 0 for success,
+		// 1 for error,
+		// 2 for fatal error,
+		// -1
+		if (b == 0)
+			return b;
+		if (b == -1)
+			return b;
+
+		if (b == 1 || b == 2) {
+			StringBuffer sb = new StringBuffer();
+			int c;
+			do {
+				c = in.read();
+				sb.append((char) c);
+			} while (c != '\n');
+			if (b == 1) { // error
+				System.out.print(sb.toString());
+			}
+			if (b == 2) { // fatal error
+				System.out.print(sb.toString());
+			}
+		}
+		return b;
 	}
 
 }
