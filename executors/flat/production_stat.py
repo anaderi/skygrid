2607451a294744b44a586c639b10fb2c7fc4de83
Andrey Ustyuzhanin <andrey.u@gmail.com>
2014-09-06 22:42:50 +0200

    prod stat update

--- executors/flat/production_stat.py
+++ executors/flat/production_stat.py
@@ -4,6 +4,8 @@ import os
 import argparse
 import logging
 from util import QueueDir
+import ordereddict
+
 
 logger = logging.getLogger()
 logger.addHandler(logging.StreamHandler())
@@ -14,8 +16,9 @@ SUCC = 'success'
 FAIL = 'fail'
 WORK = 'work'
 ORIG = 'orig'
+MISSING = 'miss'
 FAILRATE = 'f-rate'
-SUCCRATE = 'succrate'
+SUCCRATE = 'success-rate'
 TOTAL = 'total'
 
 queue_exts = {
@@ -32,6 +35,7 @@ def parse_args():
     p.add_argument("--queues", "-q", help="list of queues to compute stats for")
     p.add_argument("--basedir", "-b", help="basedir", default=".")
     p.add_argument("--verbose", "-v", action='store_true', default=False)
+    p.add_argument("--exptotal", "-e", type=int, help="expected totals per group")
     args = p.parse_args()
     if args.verbose:
         logger.setLevel(logging.DEBUG)
@@ -39,7 +43,7 @@ def parse_args():
     return args
 
 
-def stat_host(basedir, name):
+def stat_host(basedir, name, exptotal=None):
     stat = {}
     total_count = 0
     for key, suff in queue_exts.iteritems():
@@ -52,24 +56,49 @@ def stat_host(basedir, name):
             stat[key] = 0
 
     stat[TOTAL] = total_count
-    if total_count > 0:
+    update_calc_stat(stat, exptotal)
+    stat['name'] = name
+    return stat
+
+
+def update_calc_stat(stat, exptotal=None):
+    if stat[TOTAL] > 0:
         stat[FAILRATE] = 100. * stat[FAIL] / stat[TOTAL]
         stat[SUCCRATE] = 100. * stat[SUCC] / stat[TOTAL]
     else:
         stat[FAILRATE] = -1.
         stat[SUCCRATE] = -1.
-    stat['name'] = name
-    return stat
+    if exptotal is not None:
+        stat[MISSING] = exptotal - stat[TOTAL]
 
 
-def print_stat(group_stat):
-    keys    = [NAME, SUCC, FAIL, WORK, ORIG, TOTAL, FAILRATE, SUCCRATE]
-    formats = ["%s", "%d", "%d", "%d", "%d", "%d", "%.1f", "%.1f"]
-    header = "\t".join(keys)
+def print_stat(group_stat, exptotals):
+    report = ordereddict.OrderedDict([
+        (NAME, "%s"), 
+        (SUCC, "%d"), 
+        (FAIL, "%d"),
+        (WORK, "%d"),
+        (ORIG, "%d"),
+        (TOTAL, "%d"),
+        (FAILRATE, "%.1f"),
+        (SUCCRATE, "%.1f\t")
+    ])
+    if exptotals is not None:
+        report[MISSING] = "%d"
+    header = "\t".join(report.keys())
     print header
+    totals = {NAME: 'TOTAL'}
     for stat in group_stat:
-        row = "\t".join([formats[i] % stat[k] for i, k in enumerate(keys)])
+        row = "\t".join([format % stat[key] for key, format in report.iteritems()])
         print row
+        for k in [SUCC, FAIL, WORK, ORIG, TOTAL]:
+            if k in totals:
+                totals[k] += stat[k]
+            else:
+                totals[k] = stat[k]
+    update_calc_stat(totals, exptotal=len(group_stat) * exptotals)
+    totals_row = "\t".join([format % totals[key] for key, format in report.iteritems()])
+    print totals_row
 
 
 def main(args):
@@ -79,8 +108,8 @@ def main(args):
     if args.queues:
         groups = args.queues.split(",")
 
-    group_stat = [stat_host(args.basedir, group) for group in groups]
-    print_stat(group_stat)
+    group_stat = [stat_host(args.basedir, group, exptotal=args.exptotal) for group in groups]
+    print_stat(group_stat, exptotals=args.exptotal)
 
 
 if __name__ == '__main__':
