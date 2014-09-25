3eefea107ef960d3a5c7927bb21e147a78eca058
Alexander Baranov <sashab1@yandex-team.ru>
2014-09-25 18:40:11 +0400

    Update setup.py for pypi

--- metascheduler/master/setup.py
+++ metascheduler/master/setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='metascheduler',
+    name='skygrid-metascheduler',
     version='0.1.0',
-    author='Yandex',
+    url='https://github.com/anaderi/skygrid',
+    author='Alexander Baranov',
     author_email='sashab1@yandex-team.ru',
-    packages=find_packages(),
+    packages=['metascheduler', 'metascheduler.resources'],
     description='Metascheduler',
     install_requires=[
         "Flask==0.10.1",
@@ -19,12 +20,15 @@ setup(
         "flask-mongoengine==0.7.1",
         "itsdangerous==0.24",
         "mongoengine==0.8.7",
-        "nose==1.3.4",
-        "nose-testconfig==0.9",
         "pymongo==2.7.2",
         "pytz==2014.4",
         "requests==2.3.0",
         "six==1.7.3",
         "wsgiref==0.1.2",
+        'skygrid-libscheduler==0.1.0',
+    ],
+    tests_require=[
+        "nose==1.3.4",
+        "nose-testconfig==0.9",
     ],
 )
