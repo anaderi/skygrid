0290561e3d4a1a04b8e0dfe7ca61ea0cf7a9f1c7
Alexander Baranov <sashab1@yandex-team.ru>
2014-11-28 21:11:21 +0300

    Configuration of frontend

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 app = Flask(__name__)
+app.config.from_envvar('SKYGRID_FRONTEND_CONFIG')
 
 @app.route("/")
 def hello():
@@ -11,4 +12,4 @@ def datasets():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
