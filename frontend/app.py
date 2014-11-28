1e6eb2f0f23884fa1dcea6705162693d680d978e
Sasha Baranov <sashab1@yandex-team.ru>
2014-11-28 20:29:31 +0300

    Frontend initial commit

---
+++ frontend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/datasets")
+def datasets():
+    return render_template("datasets.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
