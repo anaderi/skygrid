d14e52a26e0bf055b5c95e78b2c24c85928ad3d7
Alexander Baranov <sashab1@yandex-team.ru>
2014-12-04 23:55:13 +0300

    Move tests to particular files

---
+++ skygrid/tests/common.py
@@ -0,0 +1,19 @@
+import json
+import os
+import random
+import unittest
+import uuid
+import hashlib
+
+from time import sleep
+
+import requests
+from testconfig import config
+
+class BasicSkygridTest(unittest.TestCase):
+    def setUp(self):
+        self.api_url = config['api']['url']
+        self.datasets_url = os.path.join(config['api']['url'], 'datasets')
+        self.classifiers_url = os.path.join(config['api']['url'], 'classifiers')
+        self.json_headers =  {'content-type': 'application/json'}
+
