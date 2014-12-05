fc8fdf701402af9dc45761b5ec8ba45cc6662b5f
Alexander Baranov <sashab1@yandex-team.ru>
2014-12-05 18:12:30 +0300

    MC list and submit

--- frontend/app.py
+++ frontend/app.py
@@ -1,45 +1,37 @@
 import json
-from flask import Flask, render_template
+
+from flask import Flask
+
+from views import *
 from libscheduler import Metascheduler
 
 app = Flask(__name__)
 app.config.from_envvar('SKYGRID_FRONTEND_CONFIG')
 ms = Metascheduler(app.config['METASCHEDULER_URL'])
 
-
-@app.route("/")
-def hello():
-    return render_template("index.html")
-
-
-@app.route("/datasets")
-def datasets():
-    return render_template("datasets.html")
-
-
-@app.route("/classifiers")
-def classifiers():
-    return render_template("classifiers.html")
-
-
-@app.route("/metascheduler/jobs/<job_id>")
-def ms_job(job_id):
-    job = ms.job(job_id, from_api=True)
-
-    return render_template(
-        "job.html",
-        job=job
-    )
-
-
-@app.route("/metascheduler")
-def queues():
-    return render_template("queues.html")
+app.secret_key = r'k%s&,HjU$^UMBFR(pkdfbu)*&'
 
 
 @app.template_filter('to_json')
 def to_json(value):
     return json.dumps(value, indent=4)
+
+
+
+
+
+app.add_url_rule('/', view_func=RenderTemplateView.as_view('index_page', template='index.html'))
+app.add_url_rule('/datasets', view_func=RenderTemplateView.as_view('datasets_page', template='datasets.html'))
+app.add_url_rule('/classifiers', view_func=RenderTemplateView.as_view('classifiers_page', template='classifiers.html'))
+app.add_url_rule('/metascheduler', view_func=RenderTemplateView.as_view('ms_page', template='queues.html'))
+
+
+
+app.add_url_rule('/metascheduler/jobs/<job_id>', view_func=JobView.as_view('job_page'))
+
+
+app.add_url_rule('/mc/submit', view_func=MCSubmitView.as_view('mc_submit'))
+app.add_url_rule('/mc/list', view_func=RenderTemplateView.as_view('mc_list', template='mc_list.html'))
 
 if __name__ == "__main__":
     app.run(host="cern-dev01i.vs.os.yandex.net", debug=True, port=5002)
