999430a8948eafec5bdd19e1c1f5559c0aa3f0d9
Alexander Baranov <sashab1@yandex-team.ru>
2014-12-10 22:05:56 +0300

    Update name

--- libskygrid/setup.py
+++ libskygrid/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name='libskygrid',
+    name='skygrid-libskygrid',
     version='0.1.0',
     url='https://github.com/anaderi/skygrid',
     author='Alexander Baranov',
