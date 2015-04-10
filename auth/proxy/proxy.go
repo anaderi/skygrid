82087399b4f6a4c9c68bf2ae6c8a08a5cc314251
Sasha Baranov <sashab1@yandex-team.ru>
2015-04-11 02:10:39 +0800

    Check if host is in redis; Add token owner from redis

--- auth/proxy/proxy.go
+++ auth/proxy/proxy.go
@@ -8,6 +8,9 @@ import (
     "strings"
 )
 
+const TOKEN_HEADER = "X-Yacern-Token"
+const USERNAME_HEADER = "X-Yacern-User"
+
 func main() {
     c, err := redis.Dial("tcp", ":6379")
     if err != nil {
@@ -20,9 +23,10 @@ func main() {
 
     proxy.OnRequest().DoFunc(
     func(r *http.Request,ctx *goproxy.ProxyCtx)(*http.Request,*http.Response) {
-        tokenkey := strings.Join([]string{"token:", r.Header.Get("Token")}, "")
+        token := r.Header.Get(TOKEN_HEADER)
+        tokenkey := strings.Join([]string{"token:", token}, "")
 
-        username, err := redis.String(c.Do("GET", tokenkey))
+        token_ok, err := redis.Bool(c.Do("SISMEMBER", tokenkey, r.Host))
 
         if err != nil {
             log.Fatal(err)
@@ -32,10 +36,19 @@ func main() {
                     "Internal error")
         }
 
+        if token_ok {
+            ownerkey := strings.Join([]string{"owner:", token}, "")
+            username, err := redis.String(c.Do("GET", ownerkey))
+            if err != nil {
+                log.Fatal(err)
 
-        if username != ""{
-            delete(r.Header, "Token")
-            r.Header.Set("Username", username)
+                return r,goproxy.NewResponse(r,
+                        goproxy.ContentTypeText,http.StatusInternalServerError,
+                        "Internal error")
+            }
+
+            delete(r.Header, TOKEN_HEADER)
+            r.Header.Set(USERNAME_HEADER, username)
 
             return r,nil
         } else {
@@ -48,4 +61,3 @@ func main() {
 
     log.Fatal(http.ListenAndServe(":8080", proxy))
 }
-
