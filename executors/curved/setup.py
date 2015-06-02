60a60c0f5160ceb30020b819a5beee5818fe0276
Alexander Baranov <sashab1@yandex-team.ru>
2015-06-02 19:07:14 +0300

    Use libscheduler 0.5.0

--- executors/curved/setup.py
+++ executors/curved/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skygrid-docker-worker',
-    version='0.2.10',
+    version='0.3.0',
     url='https://github.com/anaderi/skygrid',
     author='Alexander Baranov',
     author_email='sashab1@yandex-team.ru',
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "lockfile==0.10.2",
         "requests>=2.5.1",
-        "skygrid-libscheduler==0.4.3",
+        "skygrid-libscheduler==0.5.0",
         "skygrid-libskygrid==0.1.1",
         "docker-py==1.1.0",
         "six==1.9.0",
