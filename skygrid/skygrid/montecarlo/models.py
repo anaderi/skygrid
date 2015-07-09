58c6d1124d9e6b63786ff2cf661e3aae35e1ca0c
Alexander Baranov <sashab1@yandex-team.ru>
2015-07-09 13:57:30 +0300

    Remove Counter after some timeit tests. http://stackoverflow.com/questions/27801945/surprising-results-with-python-timeit-counter-vs-defaultdict-vs-dict

--- skygrid/skygrid/montecarlo/models.py
+++ skygrid/skygrid/montecarlo/models.py
@@ -1,10 +1,20 @@
 import os
 from datetime import datetime
-from collections import Counter
 
 from mongoengine import *
 
 from flask import current_app
+
+
+class JobStatus:
+    pending = "pending"
+    running = "running"
+    failed  = "failed"
+    completed = "completed"
+    pulled = "pulled"
+
+    valid_statuses = set([pending, running, failed, completed, pulled])
+
 
 class MonteCarlo(Document):
     jobs = DictField()
@@ -23,12 +33,15 @@ class MonteCarlo(Document):
     }
 
     def to_dict(self):
+        statuses = self.jobs.values()
         return {
             'montecarlo_id': str(self.pk),
             'descriptor': self.descriptor,
             'multiplier': self.multiplier,
             'created': self.created.strftime(current_app.config['TIME_FORMAT']),
-            'jobs': Counter(self.jobs.values()),
+            'jobs': {
+                status: statuses.count(status) for status in JobStatus.valid_statuses
+            },
         }
 
 
