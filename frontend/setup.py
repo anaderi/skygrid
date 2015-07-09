6acb57c944692cad747148817f957fb55a67bbba
Alexander Baranov <sashab1@yandex-team.ru>
2015-07-09 19:18:54 +0300

    Support new job stats format

--- frontend/setup.py
+++ frontend/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='skygrid-frontend',
-    version='0.2.2',
+    version='0.2.3',
     url='https://github.com/anaderi/skygrid',
     author='Alexander Baranov',
     author_email='sashab1@yandex-team.ru',
