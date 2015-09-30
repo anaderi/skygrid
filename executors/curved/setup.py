a19ea2be35391f2f5bddecdd40b1c994d5abf11d
Alexander Baranov <sashab1@yandex-team.ru>
2015-09-30 13:55:09 +0300

    Bump libscheduler version

--- executors/curved/setup.py
+++ executors/curved/setup.py
@@ -11,7 +11,7 @@ setup(
     install_requires=[
         "lockfile==0.10.2",
         "requests>=2.5.1",
-        "skygrid-libscheduler==0.5.2",
+        "skygrid-libscheduler>=0.5.7",
         "skygrid-libskygrid==0.1.1",
         "docker-py==1.1.0",
         "six==1.9.0",
