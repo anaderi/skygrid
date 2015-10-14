fb58b7b45c0c1ae3230d08a9f9a1d9e69e035a75
Alexander Baranov <sashab1@yandex-team.ru>
2015-10-14 14:42:37 +0300

    Recursive pulls for git submodules

--- executors/curved/dockerworker/worker/backend.py
+++ executors/curved/dockerworker/worker/backend.py
@@ -87,7 +87,7 @@ class WebDAVBackend(BackendBase):
 
 class GitBackend(BackendBase):
     def copy_from_backend(self, src_path, dst_path):
-        Repo.clone_from(src_path, dst_path)
+        Repo.clone_from(src_path, dst_path, recursive=True)
 
     def copy_to_backend(self, src_path, dst_path):
         raise NotImplementedError
