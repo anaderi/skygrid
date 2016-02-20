36452969859828b02b1159c63ad2e5c1c6307f94
Alexander Baranov <sashab1@yandex-team.ru>
2016-02-20 14:04:16 +0300

    XrootD backend support

--- executors/curved/skygridbackends/__init__.py
+++ executors/curved/skygridbackends/__init__.py
@@ -1,6 +1,8 @@
 import base
+
 from git import GitBackend
 from local import LocalBackend
 from webdav import WebDAVBackend
+from xrootd import XrootdBackend
 
 from multi import MultiBackend
---
+++ executors/curved/skygridbackends/xrootd.py
@@ -0,0 +1,16 @@
+from sh import xrdfs, xrdcopy
+from .base import BackendBase
+
+
+class XrootdBackend(BackendBase):
+    def __init__(self, xrd_server):
+        self.xrd_server = xrd_server
+
+    def copy_from_backend(self, src_path, dst_path):
+        xrdcopy("-r", self.xrd_server + src_path, dst_path)
+
+    def copy_to_backend(self, src_path, dst_path):
+        xrdcopy("-r", src_path, self.xrd_server + dst_path)
+
+    def list_uploaded(self, path):
+        xrdfs(self.xrdfs_server, "ls", path).split("\n")
