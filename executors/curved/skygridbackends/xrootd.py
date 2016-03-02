b205682987b142e629f5dd77d9e60936b4dad779
Alexander Baranov <sashab1@yandex-team.ru>
2016-03-02 17:28:39 +0300

    Make xrootd output directory before upload

--- executors/curved/skygridbackends/xrootd.py
+++ executors/curved/skygridbackends/xrootd.py
@@ -10,6 +10,7 @@ class XrootdBackend(BackendBase):
         xrdcopy("-r", self.xrd_server + src_path, dst_path)
 
     def copy_to_backend(self, src_path, dst_path):
+        xrdfs(self.xrdfs_server, "mkdir", "-p", dst_path)
         xrdcopy("-r", src_path, self.xrd_server + dst_path)
 
     def list_uploaded(self, path):
