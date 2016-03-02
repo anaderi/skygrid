937d58aa80912cbb6fb8ac5635f685c8a42df78a
Alexander Baranov <sashab1@yandex-team.ru>
2016-03-02 18:36:28 +0300

    Fix xrootd backend

--- executors/curved/skygridbackends/xrootd.py
+++ executors/curved/skygridbackends/xrootd.py
@@ -10,8 +10,8 @@ class XrootdBackend(BackendBase):
         xrdcopy("-r", self.xrd_server + src_path, dst_path)
 
     def copy_to_backend(self, src_path, dst_path):
-        xrdfs(self.xrdfs_server, "mkdir", "-p", dst_path)
+        xrdfs(self.xrd_server, "mkdir", "-p", dst_path)
         xrdcopy("-r", src_path, self.xrd_server + dst_path)
 
     def list_uploaded(self, path):
-        xrdfs(self.xrdfs_server, "ls", path).split("\n")
+        xrdfs(self.xrd_server, "ls", path).split("\n")
